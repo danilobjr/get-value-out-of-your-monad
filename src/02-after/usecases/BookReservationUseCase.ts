75e0bcb6d88d463ece01e19feb77303622950714
Danilo Barros <danilobjr@gmail.com>
2022-03-17 17:35:09 -0300

    refactor: use helper function

--- src/02-after/usecases/BookReservationUseCase.ts
+++ src/02-after/usecases/BookReservationUseCase.ts
@@ -1,5 +1,6 @@
 import type { Reservation } from '../models'
 import { IReservationRepo } from '../repos'
+import { add } from '../utils/mathUtils'
 
 export interface IBookReservationUseCase {
   tryAccept: (reservation: Reservation) => Promise<number | null>
@@ -11,11 +12,9 @@ export class BookReservationUseCase implements IBookReservationUseCase {
   async tryAccept(reservation: Reservation): Promise<number | null> {
     const reservations = await this.repo.getByDate(reservation.date)
 
-    // TODO refactor - use helper functions instead of write callbacks from scratch
-    // TODO refactor - even more: use transducers to loop through array only once
     const reservedSeatsQuantity = reservations
       .map((r) => r.quantity)
-      .reduce((x, y) => x + y, 0)
+      .reduce(add, 0)
 
     const overCapacity =
       reservedSeatsQuantity + reservation.quantity > this.capacity
